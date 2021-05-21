43ada0116e30057e46b0724bea9530bc45da939f
cdswyda <cdswyda@hotmail.com>
2021-05-21 09:31:33 +0800

    feature: 集成调用数据验证

--- src/controllers/epointInsert.js
+++ src/controllers/epointInsert.js
@@ -1,7 +1,9 @@
 import Store from '../store';
 import luckysheetPostil from './postil';
 import hyperlinkCtrl from './hyperlinkCtrl';
+import dataVerificationCtrl from './dataVerificationCtrl';
 import { createLuckyChart, hideAllNeedRangeShow } from '../expendPlugins/chart/plugin';
+import { getSheetIndex, getRangetxt } from '../methods/get';
 import {
     checkProtectionLockedRangeList,
     checkProtectionAllSelected,
@@ -22,6 +24,92 @@ function showFillInDescription() {
         }
     }
 }
+
+function isAllowEdit() {
+    if (!checkProtectionNotEnable(Store.currentSheetIndex)) {
+        return false;
+    }
+
+    if (Store.luckysheet_select_save == null || Store.luckysheet_select_save.length == 0) {
+        return false;
+    }
+    return true;
+}
+
+function insertHandle(key) {
+    if (!isAllowEdit()) {
+        return;
+    }
+
+    dataVerificationCtrl.createDialog();
+    dataVerificationCtrl.init();
+
+    // 单选多选
+    if (key === 'dropdown-radio' || key === 'dropdown-check') {
+        $('#data-verification-type-select')
+            .val('dropdown')
+            .trigger('change');
+
+        $('#data-verification-multi').prop('checked', key === 'dropdown-check');
+    }
+
+    // 复选
+    if (key === 'checkbox') {
+        $('#data-verification-type-select')
+            .val('checkbox')
+            .trigger('change');
+    }
+
+    // 文本
+    if (key === 'text') {
+        $('#data-verification-type-select')
+            .val('text_length')
+            .trigger('change');
+    }
+
+    // 数值
+    if (key === 'number') {
+        $('#data-verification-type-select')
+            .val('number_integer')
+            .trigger('change');
+    }
+
+    // 日期
+    if (key === 'date') {
+        $('#data-verification-type-select')
+            .val('date')
+            .trigger('change');
+    }
+
+    // 身份证
+    if (key === 'idcard') {
+        $('#data-verification-type-select')
+            .val('validity')
+            .trigger('change');
+        $('data-verification-validity-select')
+            .val('card')
+            .trigger('change');
+    }
+    // 手机号
+    if (key === 'phone') {
+        $('#data-verification-type-select')
+            .val('validity')
+            .trigger('change');
+        $('data-verification-validity-select')
+            .val('phone')
+            .trigger('change');
+    }
+
+    // const range = Store.luckysheet_select_save[Store.luckysheet_select_save.length - 1];
+    // const rangeText = getRangetxt(Store.currentSheetIndex, range, Store.currentSheetIndex);
+    // if (window.EpointSheetDesigner && window.EpointSheetDesigner.eventHandle) {
+    //     const show = window.EpointSheetDesigner.eventHandle[key];
+    //     if (typeof show === 'function') {
+    //         show(range, rangeText);
+    //     }
+    // }
+}
+
 /**
  * 插入图片
  * @returns
@@ -85,6 +173,15 @@ function insertPivotTable() {
 const handlers = {
     // 填报说明
     'fill-in-description': showFillInDescription,
+    'dropdown-radio': insertHandle,
+    'dropdown-check': insertHandle,
+    checkbox: insertHandle,
+    text: insertHandle,
+    number: insertHandle,
+    date: insertHandle,
+    idcard: insertHandle,
+    phone: insertHandle,
+
     // ======================
     image: insertImage,
     chart: insertChart,
@@ -122,9 +219,9 @@ const epointInsert = {
         { text: '手机号', value: 'phone', example: '' }
     ],
     handle: function(itemValue) {
-        const args = [].slice.call(arguments, 1);
+        // const args = [].slice.call(arguments, 1);
         if (typeof handlers[itemValue] === 'function') {
-            handlers[itemValue].apply(handlers, args);
+            handlers[itemValue].apply(handlers, arguments);
         }
     }
 };
